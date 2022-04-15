38b3517e7c2457bfe284772b3a4179247bde3b32
yuanyuancin <yuancin@163.com>
2022-04-16 02:29:11 +0800

    init

---
+++ src/main/java/com/alipay/sofa/springbootarkbiz/SpringBootArkBizApplication.java
@@ -0,0 +1,24 @@
+package com.alipay.sofa.springbootarkbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication(exclude = JacksonAutoConfiguration.class)
+public class SpringBootArkBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringBootArkBizApplication.class).web(WebApplicationType.NONE);
+		ConfigurableApplicationContext context = builder.build().run(args);
+
+//		ConfigurableApplicationContext context = SpringApplication.run(SpringBootArkBizApplication.class, args);
+		System.out.println("SpringBootArkBizApplication start!");
+		System.out.println("SpringBootArkBizApplication spring boot version: " + SpringApplication.class.getPackage().getImplementationVersion());
+		System.out.println("SpringBootArkBizApplication classLoader: " + SpringBootArkBizApplication.class.getClassLoader());
+
+	}
+}
---
+++ src/test/java/com/alipay/sofa/springbootarkbiz/SpringBootArkBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.springbootarkbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArkBizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
