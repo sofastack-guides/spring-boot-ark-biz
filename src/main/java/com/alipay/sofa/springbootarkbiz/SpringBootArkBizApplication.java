fb66e61cfd9774881be68725b84ed166a4f14dd1
youji.zzl <youji.zzl@antfin.com>
2023-06-19 11:54:36 +0800

    set biz resource loader from base classloader

--- src/main/java/com/alipay/sofa/springbootarkbiz/SpringBootArkBizApplication.java
+++ src/main/java/com/alipay/sofa/springbootarkbiz/SpringBootArkBizApplication.java
@@ -4,15 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 
-
-@SpringBootApplication(exclude = JacksonAutoConfiguration.class)
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class SpringBootArkBizApplication {
 
 	public static void main(String[] args) {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringBootArkBizApplication.class).web(WebApplicationType.SERVLET);
+
+		// set biz to use resource loader.
+		ResourceLoader resourceLoader = new DefaultResourceLoader(Thread.currentThread().getContextClassLoader());
+		builder.resourceLoader(resourceLoader);
+
 		ConfigurableApplicationContext context = builder.build().run(args);
 
 //		ConfigurableApplicationContext context = SpringApplication.run(SpringBootArkBizApplication.class, args);
